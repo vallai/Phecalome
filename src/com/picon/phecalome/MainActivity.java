1e60afc6233c92006b7009d98de18ccbd7469667
Christian <christian@cirkwi.com>
2015-02-26 22:48:24 +0100

    Activités main et caca
    Activité main :
    - Logo avec titre
    - 2 buttons "caca" et "stats"
    
    Activité caca :
    - 7 boutons sur slider vertical

--- src/com/picon/phecalome/MainActivity.java
+++ src/com/picon/phecalome/MainActivity.java
@@ -1,15 +1,32 @@
 package com.picon.phecalome;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// Evenements sur les boutons
+		Button bouton_caca = (Button) findViewById(R.id.caca_button);
+		bouton_caca.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.caca_button : 
+			Intent intentCaca = new Intent(MainActivity.this, CacaActivity.class);
+			startActivity(intentCaca);
+			break;
+		}
 	}
 }
